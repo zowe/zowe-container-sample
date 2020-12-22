944b4fa7db8b1261121fdcb1c34d83c8fc47d4c0
Nakul Manchanda <nakul.manchanda@ibm.com>
2020-12-22 10:55:02 -0500

    add shell and license header
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- build/zlux/pluginInstall.sh
+++ build/zlux/pluginInstall.sh
@@ -1,3 +1,17 @@
+#!/bin/sh
+
+#########################################################################################
+#                                                                                       #
+# This program and the accompanying materials are made available under the terms of the #
+# Eclipse Public License v2.0 which accompanies this distribution, and is available at  #
+# https://www.eclipse.org/legal/epl-v20.html                                            #
+#                                                                                       #
+# SPDX-License-Identifier: EPL-2.0                                                      #
+#                                                                                       #
+# Copyright IBM Corporation 2020                                                        #
+#                                                                                       #
+#########################################################################################
+
 if [ -d "/dropins" ]; then
   cd /dropins;
   for D in */;
